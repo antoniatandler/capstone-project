b4946321e131be973e27a4598049552a4d28cca7
Antonia Tandler <q>
2023-05-24 11:40:01 +0200

    added PosesList Component and poses page

---
+++ pages/poses/index.js
@@ -0,0 +1,11 @@
+import Heading from "@/components/Heading";
+import PosesList from "@/components/PosesList.js";
+
+export default function ListOfYogaPoses() {
+  return (
+    <>
+      <Heading />
+      <PosesList />
+    </>
+  );
+}
