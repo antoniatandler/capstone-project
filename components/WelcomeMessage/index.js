e05c6eaf69fd403c09f6d0db2b5e1b6ffce2de3f
Antonia Tandler <q>
2023-05-24 13:22:55 +0200

    added WelcomeMessage Component

---
+++ components/WelcomeMessage/index.js
@@ -0,0 +1,13 @@
+import Heading from "../Heading";
+
+export default function WelcomeMessage() {
+  return (
+    <>
+      <Heading />
+      <div className="welcome">
+        <h2>MOINI</h2>
+        <p>nice to meet you here!</p>
+      </div>
+    </>
+  );
+}
