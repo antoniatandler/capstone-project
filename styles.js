7830c882e50f9240ba29c4488fc2e6094c5b432b
Antonia Tandler <q>
2023-05-30 16:39:27 +0200

    edited global styles

--- styles.js
+++ styles.js
@@ -15,21 +15,4 @@ export default createGlobalStyle`
     padding: 15px;
     color: #59311f;
   }
-
-  h1 {
-    padding-top: 15px;
-    text-align: center;
-    border: dotted;
-    font-size: 40px;
-    margin: auto;
-    position: fixed;
-    top: 10px;
-    left: 50%;
-    translate: -50%;
-    background-color: #dbf3e1;
-    width: 95%;
-    color: #dbb290;
-    border-radius: 15px;
-    letter-spacing: 5px;
-  }
 `;
