040dc0098c411b44c44033c7c3378cef81388533
Antonia Tandler <q>
2023-06-03 03:35:50 +0200

    created Hamburger Menue and exported/imported it

--- components/Heading.js
+++ components/Heading.js
@@ -1,23 +1,28 @@
-import React from "react";
 import styled from "styled-components";
+import HamburgerMenue from "./HamburgerMenue";
 
 const MainHeading = styled.h1`
   padding-top: 15px;
   text-align: center;
   border: dotted;
-  font-size: 40px;
+  font-size: 30px;
   margin: auto;
   position: fixed;
-  top: 10px;
+  top: 30px;
   left: 50%;
   translate: -50%;
   background-color: #dbf3e1;
-  width: 95%;
+  width: 70%;
   color: #dbb290;
   border-radius: 15px;
   letter-spacing: 5px;
 `;
 
 export default function Heading() {
-  return <MainHeading>ANTONYOGA</MainHeading>;
+  return (
+    <>
+      <HamburgerMenue />
+      <MainHeading>ANTONYOGA</MainHeading>
+    </>
+  );
 }
