90322295fef46b94cc36d3edada3395eadc585b1
Antonia Tandler <q>
2023-06-16 03:06:36 +0200

    deleted localStorage.js

--- util/LocalStorage.js
+++
@@ -1,8 +0,0 @@
-export function loadFromLocalStorage(key) {
-  const value = localStorage.getItem(key);
-  return JSON.parse(value);
-}
-
-export function writeToLocalStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
-}
